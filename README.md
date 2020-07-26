4f6fa6f995387c34361ab4203d912242835459b1
Manabu TERADA <terada@cmscom.jp>
2020-07-26 10:09:39 +0900

    README

--- README.md
+++ README.md
@@ -2,8 +2,46 @@
 
 ## Install
 
+### PyQt
+
+/src/
+
 ```
 $ sudo apt-get install qttools5-dev-tools
 (env) $ pip install PyQt5
 (env) $ pip install aiohttp
+```
+
+### Eel
+
+/front/
+
+```
+(env) $ pip install eel
+```
+
+### Vuex + Eel
+
+/dic/
+
+```
+$ vue create front
+$ cd front
+$ vue add vuetify
+```
+
+```
+ $ cd front
+ $ npm run serve
+ $ npm run build
+```
+
+```
+$ python3 -m venv env
+$ . env/bin/activate
+(env) $ pip install eel
+```
+
+```
+(env) $ python gui.py
 ```
--- src/book-demo/echoserver1.py
+++ src/book-demo/echoserver1.py
@@ -8,6 +8,7 @@ logger = logging.getLogger()  #  Added
 
 async def websocket_handler(request):
     ws = web.WebSocketResponse()
+    ws.headers['Access-Control-Allow-Origin'] = '*'
     await ws.prepare(request)
 
     async for msg in ws:
@@ -22,9 +23,17 @@ async def websocket_handler(request):
     return ws
 
 
+@web.middleware
+async def cors_middleware(request, handler):
+    response = await handler(request)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
+
+
 app = web.Application()  #  Added
 app.add_routes(
-    [web.get("/ws", websocket_handler),]
+    [web.get("/ws", websocket_handler),
+     web.get("/calc", cors_middleware),]
 )
 
-web.run_app(app, port=8080)
+web.run_app(app, port=8000)
